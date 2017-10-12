2eece3447263da9821f42a4d415d31a6ab7c1810
John Anderson <madroxprime@gmail.com>
2017-10-12 11:24:10 -0500

    Rough test

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
-const rc = require("./index.js");
+const rc = require('./index.js');
 
-rc("./notReal", function(err,data){
-    if(err) console.log(err);
+rc('./notReal', function(err,data){
+    if(err) {
+        console.log(err);
+    }
     if(data){
         console.log(data.name);
     }
-})
+});
